d1297d74578ee95658be83e4d8eaf1716081ccba
diana gomez <diaartgo@gmail.com>
2022-09-12 19:28:19 -0500

    implementando lectura de rutas

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+const path = require('node:path');
+const fs = require('fs');
+
+const route = './README.md'
+// const mdLinks = (ruta)=> {
+// }
+let pathAbsolute;
+const routeValidator = (route) => {
+
+  const rutaExiste=fs.existsSync(route);
+  // devuelve un true o false
+   if (rutaExiste==true){
+  
+    //ver si un link es absoluto 
+    const tipoRuta=path.isAbsolute(route);
+
+    //convertir una ruta relativa en absoluta
+    if (tipoRuta == false){
+      pathAbsolute = path.resolve(route);
+   
+    } else pathAbsolute=tipoRuta
+
+    //como sacar la extension que tiene un archivo
+    const pathMd = path.extname(pathAbsolute);
+
+      if (pathMd =='.md'){
+        return pathAbsolute
+        
+      }
+   }
+  
+}
+
+console.log(routeValidator(route));
+
+
+// const leerArchivo = (rutaValidada) => {
+//   fs.readFileSync(rutaValidada, 'utf-8');
+//   console.log(leerArchivo)
+
+// }
+
+
+
+console.log(fs.readFileSync(routeValidator(route), 'utf-8'));
